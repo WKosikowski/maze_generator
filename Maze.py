969f05b3649937b6e53fa6438de05ab3e430494c
Wojciech Kosikowski <wkosikowski@gmail.com>
2022-12-16 14:41:31 +0000

    deleted direction variable and changed link and randomise algorythm

--- Maze.py
+++ Maze.py
@@ -18,24 +18,24 @@ class Maze:
             self.maze.append(row)
         print(self.maze)
 
-    def randomise(self, y, x):
+    def randomise(self, node):
 
         randList = []
-        if x > 0:
-            if not self.maze[y][x - 1].visited:
-                randList.append("Left")
+        if node.posX > 0:
+            if not self.maze[node.posY][node.posX - 1].visited:
+                randList.append(self.maze[node.posY][node.posX - 1])
 
-        if y > 0:
-            if not self.maze[y - 1][x].visited:
-                randList.append("Up")
+        if node.posY > 0:
+            if not self.maze[node.posY - 1][node.posX].visited:
+                randList.append(self.maze[node.posY - 1][node.posX])
 
-        if x < self.width - 1:
-            if not self.maze[y][x + 1].visited:
-                randList.append("Right")
+        if node.posX < self.width - 1:
+            if not self.maze[node.posY][node.posX + 1].visited:
+                randList.append(self.maze[node.posY][node.posX + 1])
 
-        if y < self.height - 1:
-            if not self.maze[y + 1][x].visited:
-                randList.append("Down")
+        if node.posY < self.height - 1:
+            if not self.maze[node.posY + 1][node.posX].visited:
+                randList.append(self.maze[node.posY + 1][node.posX])
 
         if len(randList) == 1:
             return randList[0]
@@ -45,25 +45,73 @@ class Maze:
             returning = randList[randNum]
             return returning
         else:
-            return "null"
+            return None
+    # def randomise(self, y, x):
+    #
+    #     randList = []
+    #     if x > 0:
+    #         if not self.maze[y][x - 1].visited:
+    #             randList.append("Left")
+    #
+    #     if y > 0:
+    #         if not self.maze[y - 1][x].visited:
+    #             randList.append("Up")
+    #
+    #     if x < self.width - 1:
+    #         if not self.maze[y][x + 1].visited:
+    #             randList.append("Right")
+    #
+    #     if y < self.height - 1:
+    #         if not self.maze[y + 1][x].visited:
+    #             randList.append("Down")
+    #
+    #     if len(randList) == 1:
+    #         return randList[0]
+    #     elif len(randList) > 1:
+    #         randNum = random.randint(0, len(randList) - 1)
+    #
+    #         returning = randList[randNum]
+    #         return returning
+    #     else:
+    #         return "null"
 
-    def link(self, direction, yOrigin, xOrigin):
-        if direction == "Right":
-            self.maze[yOrigin][xOrigin].rightNode = self.maze[yOrigin][xOrigin + 1]
-            self.maze[yOrigin][xOrigin + 1].leftNode = self.maze[yOrigin][xOrigin]
-            print("movedTo " + "Right")
-        if direction == "Left":
-            self.maze[yOrigin][xOrigin].leftNode = self.maze[yOrigin][xOrigin - 1]
-            self.maze[yOrigin][xOrigin - 1].rightNode = self.maze[yOrigin][xOrigin]
-            print("movedTo " + "Left")
-        if direction == "Down":
-            self.maze[yOrigin][xOrigin].downNode = self.maze[yOrigin + 1][xOrigin]
-            self.maze[yOrigin + 1][xOrigin].upNode = self.maze[yOrigin][xOrigin]
-            print("movedTo " + "Down")
-        if direction == "Up":
-            self.maze[yOrigin][xOrigin].upNode = self.maze[yOrigin - 1][xOrigin]
-            self.maze[yOrigin - 1][xOrigin].downNode = self.maze[yOrigin][xOrigin]
-            print("movedTo " + "Up")
+    def link(self, nextNode, originNode):
+        if originNode.posX == nextNode.posX:
+            if originNode.posY < nextNode.posY:
+                originNode.downNode = nextNode
+                nextNode.upNode = originNode
+                print("movedTo " + "Down")
+            elif originNode.posY > nextNode.posY:
+                originNode.upNode = nextNode
+                nextNode.downNode = originNode
+                print("movedTo " + "Up")
+        elif originNode.posY == nextNode.posY:
+            if originNode.posX < nextNode.posX:
+                originNode.rightNode = nextNode
+                nextNode.leftNode = originNode
+                print("movedTo " + "Right")
+            if originNode.posX > nextNode.posX:
+                originNode.leftNode = nextNode
+                nextNode.rightNode = originNode
+                print("movedTo " + "Left")
+
+
+        # if direction == "Right":
+        #     self.maze[yOrigin][xOrigin].rightNode = self.maze[yOrigin][xOrigin + 1]
+        #     self.maze[yOrigin][xOrigin + 1].leftNode = self.maze[yOrigin][xOrigin]
+        #     print("movedTo " + "Right")
+        # if direction == "Left":
+        #     self.maze[yOrigin][xOrigin].leftNode = self.maze[yOrigin][xOrigin - 1]
+        #     self.maze[yOrigin][xOrigin - 1].rightNode = self.maze[yOrigin][xOrigin]
+        #     print("movedTo " + "Left")
+        # if direction == "Down":
+        #     self.maze[yOrigin][xOrigin].downNode = self.maze[yOrigin + 1][xOrigin]
+        #     self.maze[yOrigin + 1][xOrigin].upNode = self.maze[yOrigin][xOrigin]
+        #     print("movedTo " + "Down")
+        # if direction == "Up":
+        #     self.maze[yOrigin][xOrigin].upNode = self.maze[yOrigin - 1][xOrigin]
+        #     self.maze[yOrigin - 1][xOrigin].downNode = self.maze[yOrigin][xOrigin]
+        #     print("movedTo " + "Up")
 
 
     def generate(self):
@@ -76,19 +124,20 @@ class Maze:
             currentX = current.posX
             i = i + 1
             print(i, currentX, currentY)
-            direction = self.randomise(currentY, currentX)
-            if direction != "null":
-                self.link(direction, currentY, currentX)
-                if direction == "Up":
-                    currentY -= 1
-                elif direction == "Left":
-                    currentX -= 1
-                elif direction == "Down":
-                    currentY += 1
-                elif direction == "Right":
-                    currentX += 1
-                # self.link(direction, currentY, currentX)
-                self.history.append(self.maze[currentY][currentX])
+            nextNode = self.randomise(current)
+            if nextNode != None:
+                self.link(nextNode, current)
+                # if nextNode == "Up":
+                #     currentY -= 1
+                # elif nextNode == "Left":
+                #     currentX -= 1
+                # elif nextNode == "Down":
+                #     currentY += 1
+                # elif nextNode == "Right":
+                #     currentX += 1
+                # self.link(nextNode, currentY, currentX)
+                self.history.append(nextNode)
+                # self.history.append(self.maze[currentY][currentX])
             else:
                 if len(self.history) > 0:
                     del self.history[len(self.history) - 1]
